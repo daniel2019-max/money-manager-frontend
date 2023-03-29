22ba204c2b60fa35d8f13914e10cdbd9dc46c133
Frank Leonardo Puma Chara <leonardo.chara15@gmail.com>
2023-03-28 19:55:38 -0500

    setup

---
+++ android/app/src/main/java/com/frankydaniel/misprestamos/MainActivity.java
@@ -0,0 +1,5 @@
+package com.frankydaniel.misprestamos;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.frankydaniel.misprestamos',
+  appName: 'mis-prestamos',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
